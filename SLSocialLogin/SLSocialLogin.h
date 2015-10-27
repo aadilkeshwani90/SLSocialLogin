bf6e3cba38e0bc8a46ff9ac2948190f45a2eecd5
aadil <aadil@zaptechsolutions.com>
2015-10-27 15:26:04 +0530

    Initial Commit

---
+++ SLSocialLogin/SLSocialLogin.h
@@ -0,0 +1,19 @@
+//
+//  SLSocialLogin.h
+//  SLSocialLogin
+//
+//  Created by aadil on 27/10/15.
+//  Copyright © 2015 zaptechsolutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SLSocialLogin.
+FOUNDATION_EXPORT double SLSocialLoginVersionNumber;
+
+//! Project version string for SLSocialLogin.
+FOUNDATION_EXPORT const unsigned char SLSocialLoginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SLSocialLogin/PublicHeader.h>
+
+
